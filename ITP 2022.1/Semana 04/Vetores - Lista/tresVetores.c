b22a48008a5fee03f5e386e55881e2caefff819f
Paulo Henrique Santos <phqnrz@gmail.com>
2022-05-05 13:49:14 -0300

    Update tresVetores.c

--- ITP 2022.1/Semana 04/Vetores - Lista/tresVetores.c
+++ ITP 2022.1/Semana 04/Vetores - Lista/tresVetores.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 
-int main (void) {
-
+int main(void) {
+  int A[20];
+  int i;
     
+  for(i = 0; i < 20; i++){
+    scanf("%d", &A[i]);
+  }
 
+  printf("\na)");
+  for(i = 0; i < 20; i++) {
+    if(A[i] % 3 == 0) {
+      printf("%d", A[i]);
+      if(i != (20 -1)){
+        printf(",");
+      }
+    }
+  }
+  printf("\nb)");
+  for(i = 0; i < 20; i++) {
+    if(A[i] % 4 == 0) {
+      printf("%d", A[i]);
+      if(i != (20 -1)){
+        printf(",");
+      }
+    }
+  }
 
+  printf("\nc)");
+  for(i = 0; i < 20; i++) {
+    if((A[i] % 3 != 0) && (A[i] % 4 != 0)) {
+      printf("%d", A[i]);
+      if(i != (20 - 1)){
+        printf(",");
+      }
+    }
+  }
+  return 0;
 }
